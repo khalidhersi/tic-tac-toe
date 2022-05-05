d89531622b5bfbd7b698c6c32007f9ce7853a494
khalidhersi <cipherpolfoundation@gmail.com>
2022-05-05 13:31:00 +0100

    game complete

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,85 +1,32 @@
 import "./Board.css";
-import React, { useState } from "react";
+import React from "react";
 import Square from "../Square/Square";
 
-const Board = () => {
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const [xTurn, setXTurn] = useState(true);
-  const [win, setWin] = useState(false);
+const Board = (props) => {
+  const {squares, handleClick  } = props
 
-  // onClick function triggers conditional statment & implements state hooks
-  const handleClick = (index) => {
-    if (calculateWinner(squares) || squares[index]) {
-      return;
-    }
-    // conditional statment decides if square value containes X or O
-    if (xTurn) {
-      squares[index] = "X";
-    } else {
-      squares[index] = "O";
-    }
-    setSquares(squares);
-    setXTurn(!xTurn);
-  };
-
-  // Array of Each Type of directional win
-  const allHorizontalWins = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-  ];
-
-  const allVerticalWins = [
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-  ];
-  const allDiaganalWins = [
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-
-  // Array holding all Winning Combinations
-  const winningCombinations = [];
-
-  // Pushing each directional Win into winningCombinations Array
-  allHorizontalWins.map((horizontalWin) => winningCombinations.push(horizontalWin));
-  allVerticalWins.map((verticalWin) => winningCombinations.push(verticalWin));
-  allDiaganalWins.map((diaganalWin) => winningCombinations.push(diaganalWin));
-
-
-  // Win Logic
-  const calculateWinner = (squares) => {
-    for (let i = 0; i < winningCombinations.length; i++) {
-      const [a, b, c] = winningCombinations[i];
-      if (
-        squares[a] &&
-        squares[a] === squares[b] &&
-        squares[a] === squares[c]
-      ) {
-        return squares[a];
-      }
-    }
-    return null;
-  };
-
+// Square component JSX
+  const squareJSX = (index) => {
+    return <Square value={squares[index]} onClick={() => handleClick(index)} />
+  }
+   
   // Creating board
   return (
     <div className="board">
       <div className="board__row">
-        <Square value={squares[0]} onClick={() => handleClick(0)} />
-        <Square value={squares[1]} onClick={() => handleClick(1)} />
-        <Square value={squares[2]} onClick={() => handleClick(2)} />
+        {squareJSX(0)}
+        {squareJSX(1)}
+        {squareJSX(2)}
       </div>
       <div className="board__row">
-        <Square value={squares[3]} onClick={() => handleClick(3)} />
-        <Square value={squares[4]} onClick={() => handleClick(4)} />
-        <Square value={squares[5]} onClick={() => handleClick(5)} />
+        {squareJSX(3)}
+        {squareJSX(4)}
+        {squareJSX(5)}
       </div>
       <div className="board__row">
-        <Square value={squares[6]} onClick={() => handleClick(6)} />
-        <Square value={squares[7]} onClick={() => handleClick(7)} />
-        <Square value={squares[8]} onClick={() => handleClick(8)} />
+        {squareJSX(6)}
+        {squareJSX(7)}
+        {squareJSX(8)}      
       </div>
     </div>
   );
--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -1,12 +1,88 @@
 import "./Game.css";
-import React from 'react'
+import React, { useState } from 'react'
 import Board from "../Board/Board"
 
 const Game = () => {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xTurn, setXTurn] = useState(true);
+
+  // onClick function triggers conditional statment & implements state hooks
+  const handleClick = (index) => {
+    // if Winning Combination is found Game ends
+    if (isWinner(squares) || squares[index]) {
+      return;
+    }
+    // conditional statment decides if square value containes X or O
+    if (xTurn) {
+      squares[index] = "X";
+    } else {
+      squares[index] = "O";
+    }
+    setSquares(squares);
+    setXTurn(!xTurn);
+  };
+
+  // Array of Each Type of directional win
+  const allHorizontalWins = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+  ];
+
+  const allVerticalWins = [
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+  ];
+  const allDiaganalWins = [
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  // Array holding all Winning Combinations
+  const winningCombinations = [];
+
+  // Pushing each directional Win into winningCombinations Array
+  allHorizontalWins.map((horizontalWin) => winningCombinations.push(horizontalWin));
+  allVerticalWins.map((verticalWin) => winningCombinations.push(verticalWin));
+  allDiaganalWins.map((diaganalWin) => winningCombinations.push(diaganalWin));
+
+
+  // Win Logic
+  const isWinner = (squares) => {
+    for (let i = 0; i < winningCombinations.length; i++) {
+      const [a, b, c] = winningCombinations[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  };
+
+// Dynamically changes Text on Screen to show whose turn it is and who won.
+  const winner = isWinner(squares)
+  let screenText = ``
+  if(winner){
+    screenText = `${winner} is the Winner!!!`
+  }
+  else {
+    if (xTurn) {
+      screenText = `It's X's turn.`
+    } else {
+      screenText = `It's O's turn.`
+    }
+  }
+  
   return (
     <div className="game">
       <h1 className="game__title">Tic-Tac-Toe</h1>
-      <Board />
+      <h2 className="game__text">{screenText}</h2>
+      <Board squares={squares} handleClick={handleClick}/>
+      <button className="game__btn" onClick={() => {setSquares(Array(9).fill(null))}}>Reset</button>
     </div>
   )
 }
